7fe12e92744235aa336179a3a98ece9ad7c9d961
rpsrivastava24337 <rpsrivastava24337@gmail.com>
2024-02-07 16:33:54 +0530

    Add- change feature for add in new box

---
+++ app.js
@@ -0,0 +1,93 @@
+let lists = document.getElementsByClassName("list")
+let right = document.getElementById("right")
+
+let left = document.getElementById("left")
+
+
+
+function onDrag(e) {
+    e.dataTransfer.setData("text", e.target.id)
+
+
+}
+
+
+
+function onDrop(e) {
+
+    e.preventDefault()
+    var itemId = e.dataTransfer.getData("text")
+    if (e.target == right) {
+        right.appendChild(document.getElementById(itemId))
+    }
+    else if (e.target == left) {
+        left.appendChild(document.getElementById(itemId))
+
+    }
+}
+
+
+
+function allowDrop(e) {
+    e.preventDefault()
+
+
+}
+
+
+
+// let lists = document.getElementsByClassName("list")
+// let right = document.getElementById("right")
+
+// let left = document.getElementById("left")
+
+
+
+// function onDrag(e) {
+//     e.dataTransfer.setData("text", e.target.id)
+
+
+// }
+
+
+
+// function onDrop(e) {
+
+//     e.preventDefault()
+//     var itemId = e.dataTransfer.getData("text")
+//     if (e.target == right) {
+//         right.appendChild(document.getElementById(itemId))
+//     }
+//     else if (e.target == left) {
+//         left.appendChild(document.getElementById(itemId))
+
+//     }
+// }
+
+
+
+// function allowDrop(e) {
+//     e.preventDefault()
+
+
+// }
+
+
+// let lists = document.getElementsByClassName("list")
+// let right = document.getElementById("right")
+// let left = document.getElementById("left")
+
+// function onDrag(e) {
+//     e.dataTransfer.setData("text", e.target.id)
+// }
+
+// function onDrop(e) {
+//     e.preventDefault()
+//     var itemId = e.dataTransfer.getData("text")
+//     e.target.appendChild(document.getElementById(itemId))
+// }
+// function allowDrop(e){
+//     e.preventDefault()
+
+
+// }
